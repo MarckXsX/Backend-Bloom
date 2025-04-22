800a0755052c392ba03d8a76fd33d6240be12d21
Marco Gerardo Serrano Lopez <marcolopez121@outlook.com>
2025-04-21 23:34:49 -0600

    Usuario Servicio

---
+++ src/main/java/com/HospitalBloom/BackendNotas/service/impl/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package com.HospitalBloom.BackendNotas.service.impl;
+
+import com.HospitalBloom.BackendNotas.entities.User;
+import com.HospitalBloom.BackendNotas.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class UserServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(()-> new UsernameNotFoundException("User not Found."));
+
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(user.getRole().getName().toString());
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUserName(),
+                user.getPassword(),
+                Collections.singleton(authority)
+        );
+    }
+
+    public boolean existsByUserName(String userName){
+        return userRepository.existsByUserName(userName);
+    }
+
+    public  void save(User user){
+        userRepository.save(user);
+    }
+}
