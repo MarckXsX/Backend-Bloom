efbfc235ee3fe8c6667064052a5ea5103258c1af
Marco Gerardo Serrano Lopez <marcolopez121@outlook.com>
2025-04-21 23:35:04 -0600

    Etiqueta repository

--- src/main/java/com/HospitalBloom/BackendNotas/repositories/CambioDiagnosticoRepository.java
+++ src/main/java/com/HospitalBloom/BackendNotas/repositories/CambioDiagnosticoRepository.java
@@ -2,6 +2,8 @@ package com.HospitalBloom.BackendNotas.repositories;
 
 import com.HospitalBloom.BackendNotas.entities.CambioDiagnostico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CambioDiagnosticoRepository extends JpaRepository<CambioDiagnostico,Long> {
 }
