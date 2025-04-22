19e210df3158b06854df3e0813fca07045cd1ce2
Marco Gerardo Serrano Lopez <marcolopez121@outlook.com>
2025-04-21 23:34:36 -0600

    Repositorio para el usuario

---
+++ src/main/java/com/HospitalBloom/BackendNotas/repositories/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.HospitalBloom.BackendNotas.repositories;
+
+import com.HospitalBloom.BackendNotas.entities.Role;
+import com.HospitalBloom.BackendNotas.enums.RoleList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role,Integer> {
+
+    Optional<Role> findByName(RoleList name);
+}
