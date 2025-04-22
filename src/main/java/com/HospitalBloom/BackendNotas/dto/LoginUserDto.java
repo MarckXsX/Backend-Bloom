ab7eaf358d7eb304eeb7c40f45b93ad0deb4996b
Marco Gerardo Serrano Lopez <marcolopez121@outlook.com>
2025-04-21 23:33:07 -0600

    Dto Login y Nuevo Registro

---
+++ src/main/java/com/HospitalBloom/BackendNotas/dto/LoginUserDto.java
@@ -0,0 +1,14 @@
+package com.HospitalBloom.BackendNotas.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginUserDto {
+
+    private String userName;
+    private String password;
+}
